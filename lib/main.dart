f1867c1f1d8e18e810187b9b35df31d61fe7212a
Madhav <madhavanadkat67@gmail.com>
2022-06-11 21:03:19 +0530

    step-25

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,10 @@ void main() {
         child: const HomePage(),
       ),
       routes: {
-        loginRoute: (context) => const LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        notesRoute: (context) => const NotesView(),
-        verifyEmailRoute: (context) => const VerifyEmailView(),
+        //  loginRoute: (context) => const LoginView(),
+        //registerRoute: (context) => const RegisterView(),
+        //notesRoute: (context) => const NotesView(),
+        // verifyEmailRoute: (context) => const VerifyEmailView(),
         createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
@@ -48,6 +48,8 @@ class HomePage extends StatelessWidget {
           return const VerifyEmailView();
         } else if (state is AuthStateLoggedOut) {
           return const LoginView();
+        } else if (state is AuthStateRegistering) {
+          return const RegisterView();
         } else {
           return const Scaffold(
             body: CircularProgressIndicator(),
